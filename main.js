25ddce39567c3286a529b8519a1276fb9769a2b7
Jarek Janowski <jarjanowski95@gmail.com>
2020-05-05 17:49:19 +0200

    changed footer by js

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const age = ('24');
 
 // alert('Witaj!')
 console.log(`Cześć! Jestem ${imie}, mam ${age} lata i witam Cie na mojej stronie :)`);
+
+// const heading = document.querySelector('.main-heading--js');
+
+// heading.innerHTML = `Cześć! Jestem ${imie}, mam ${age} lata i witam Cie na mojej stronie :)`;
+
+const footer = document.querySelector('.footer__decription--js');
+
+footer.innerHTML = '<b>WTF</b> - all rights reserved ';
