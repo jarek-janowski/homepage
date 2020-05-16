ffafa5b41e8514c1ed4d4002d0d135df9947082a
Jarek Janowski <jarjanowski95@gmail.com>
2020-05-16 18:21:27 +0200

    added helloAlert in JS

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-const imie = ('Jarek');
+const firstName = ('Jarek');
 const age = ('24');
 
 // alert('Witaj!')
-console.log(`Cześć! Jestem ${imie}, mam ${age} lata i witam Cie na mojej stronie :)`);
+
 
 // const heading = document.querySelector('.main-heading--js');
 
 // heading.innerHTML = `Cześć! Jestem ${imie}, mam ${age} lata i witam Cie na mojej stronie :)`;
 
-const footer = document.querySelector('.footer__decription--js');
+//---------------------------------<>------------------------------------------
 
-footer.innerHTML = '<b>WTF</b> - all rights reserved ';
+// const footer = document.querySelector('.footer__decription--js');
+
+// footer.innerHTML = '<b>WTF</b> - all rights reserved ';
+
+
+
+function hello(firstName, age) {
+    console.log((`Cześć! Jestem ${firstName}, mam ${age} lata i witam Cie na mojej stronie :)`));
+    // return firstName, age;
+}
+
+hello(firstName, age);
+hello('Konrad', 32);
+
+
+//-------------------------------<>------------------------------------------------
+
+
+function replaceText(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+replaceText('.footer__decription--js', '<b>WTF</b> - all rights reserved ');
+
+function helloAlert(firstName, age) {
+    alert(`Cześć! Jestem ${firstName}, mam ${age} lata i witam Cie na mojej stronie :)`);
+    // return firstName, age;
+}
+
+helloAlert(firstName, age);
